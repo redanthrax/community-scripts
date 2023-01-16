e818239481c2694b62932669e6967903deae437c
red <red@mac.local>
2023-01-16 13:49:34 -0800

    improved version check
    version detection improved

--- scripts_wip/Mac_Duo_Install.sh
+++ scripts_wip/Mac_Duo_Install.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "This script must be run as root."
     exit
@@ -37,7 +68,10 @@ fi
 if [ -z "$ikey" ] || [ -z "$skey" ] || [ -z "$api_hostname" ]; then
     echo "Usage: ./Mac_Duo_Install.sh -i IKEY -s SKEY -a ApiHostname"
 else
-    if (( $(echo "$(sw_vers -productVersion) > 12.9" | bc))); then
+    prodver="$(sw_vers -productVersion)"
+    vercomp $prodver "12.9"
+    vercode="$?"
+    if [[ $vercode == "1" ]]; then
         echo "Duo is not compatible with this version of MacOS."
         exit 0
     fi
